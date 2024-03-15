17153c3bbb4293f189827fc3943e2b8dbb5cfc62
Vikash Saini <154866810+VikashSaini78@users.noreply.github.com>
2024-03-15 09:29:11 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,29 @@
+//  var a = "vicky"
+//  console.log(a)
+
+
+//  function myfn(){
+//     console.log(a)
+//  }
+//  myfn()
+
+
+// function myfn()
+
+// {
+
+//     let value ="jaitpuriya"
+//     console.log(value)
+// }
+
+// myfn()
+
+// console.log(value)
+
+
+// function myfn(a,b,c){
+//         console.log(3+2+ "7")
+// }
+// myfn(3+2+"7")
+
+
